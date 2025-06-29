0884d91e5c0042643ee3ed570a90797b2e9e07c7
hieudnhe181465 <hieudnhe181465@fpt.edu.vn>
2025-06-29 16:38:43 +0700

    v1 manage booking

--- src/java/controller/BookingCrud.java
+++ src/java/controller/BookingCrud.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package controller;
 
 import entity.Booking;
@@ -19,36 +18,39 @@ import java.util.List;
  *
  * @author Admin
  */
-@WebServlet(name="BookingCrud", urlPatterns={"/bookingcrud"})
+@WebServlet(name = "BookingCrud", urlPatterns = {"/bookingcrud"})
 public class BookingCrud extends HttpServlet {
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet BookingCrud</title>");  
+            out.println("<title>Servlet BookingCrud</title>");
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet BookingCrud at " + request.getContextPath () + "</h1>");
+            out.println("<h1>Servlet BookingCrud at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
-    } 
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -56,15 +58,16 @@ public class BookingCrud extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        List<Booking> listBooking = new dao.BookingDao().getBookings(5,5, null, null, null, true, 0, 0, false);
-        request.getSession().setAttribute("listBooking", listBooking);
-        
-        request.getRequestDispatcher("profile/historybooking.jsp").forward(request, response);
-    } 
+            throws ServletException, IOException {
+        List<Booking> listBooking = new dao.BookingDao().getBookings(3, 5, null, null, null, true, 0, 0, false);
+        request.setAttribute("listBooking", listBooking); 
+        request.getRequestDispatcher("bookingmanage.jsp").forward(request, response);
 
-    /** 
+    }
+
+    /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -72,12 +75,13 @@ public class BookingCrud extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override
